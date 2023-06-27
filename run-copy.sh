4db7f3cfbd3c23cb109d050c8b7d3f8a43b7d26d
Josh Espinoza <joshespi@gmail.com>
2023-06-27 10:17:46 -0600

    modified uploading user

--- run-copy.sh
+++ run-copy.sh
@@ -4,6 +4,6 @@ docker run --name dir-check --mount source=dir-check,target=/app/result dir-chec
 
 docker cp dir-check:/app/result/directory_report.txt ~/dir-check-results/
 
-scp /home/webadmin/dir-check-results/directory_report.txt webadmin@158.91.1.123:/var/www/html/public_html/wp-content/uploads/bad-emails/docker-directory_report.txt
+scp /home/webadmin/dir-check-results/directory_report.txt webupload@158.91.1.123:/var/www/html/public_html/wp-content/uploads/bad-emails/docker-directory_report.txt
 
 docker rm dir-check
