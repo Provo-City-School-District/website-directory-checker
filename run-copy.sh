cec062d22a0f8bd9b417cdc1a195859127678f0b
Josh Espinoza <joshespi@gmail.com>
2024-09-27 09:48:40 -0600

    should work to push to graylog, but graylog doesn't appear to be working so cant properly test yet.

--- run-copy.sh
+++ run-copy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker run --name dir-check --mount source=dir-check,target=/app/result dir-check
+GRAYLOG_SERVER="158.91.5.82"
+GRAYLOG_PORT="10517"
+
+docker run --name dir-check \
+  --log-driver=gelf \
+  --log-opt gelf-address=udp://$GRAYLOG_SERVER:$GRAYLOG_PORT \
+  dir-check
 
 docker rm dir-check
